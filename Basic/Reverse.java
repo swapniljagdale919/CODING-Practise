b5123ffe699de9359befdbfaaf8c2bdfcf97de20
Swapnil Jagdale <59247013+swapniljagdale919@users.noreply.github.com>
2022-05-01 10:55:29 +0530

    Update Reverse.java

--- Basic/Reverse.java
+++ Basic/Reverse.java
@@ -1,1 +1,34 @@
+import java.util.*;
 
+class Reverse {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int reverse = reverseNum(num);
+
+        System.out.println(reverse);
+
+    }
+
+    // TODO: Implement this method
+    static int reverseNum(int num) {
+
+        // i = 10  ,, 0 =1
+        // 876 -- 678
+
+        // take given no in loop
+        //extract last digit
+        // make reverse number using that rev*10 + last digit
+        //divide by 10 to original number
+        int lastDigit = 1 , revNum=0;
+
+        while(num > 0){
+            lastDigit = num%10;
+            revNum = revNum*10 + lastDigit;
+            num/=10;
+        }
+        return revNum;
+    }
+}
